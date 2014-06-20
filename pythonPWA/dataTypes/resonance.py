447b28f6f384eb03144fa1427d2e97c729b7d2f1
bdell <brandon.demello@gmail.com>
2014-06-20 09:57:51 -0400

    added phase motion

--- pythonPWA/dataTypes/resonance.py
+++ pythonPWA/dataTypes/resonance.py
@@ -2,11 +2,12 @@ class resonance():
     """
     This class represents a resonance.
     """
-    def __init__(self,cR=1.0,wR=[],w0=1.,r0=.5):
+    def __init__(self,cR=1.0,wR=[],w0=1.,r0=.5,phase=0.):
         self.wR=wR
         self.cR=cR
         self.w0=w0
         self.r0=r0
+        self.phase=phase
 
     def toString(self):
         """
--- pythonPWA/model/complexV.py
+++ pythonPWA/model/complexV.py
@@ -6,5 +6,5 @@ def complexV(resonance,wave,waves,normint,mass):
     """
     Returns the complex value of the production amplitude for a resonance, wave in the set of waves, normalization integral, and mass.
     """
-    #return numpy.complex(magV(mass,resonance,wave,waves,normint)*numpy.cos(getPhi(mass,resonance)),magV(mass,resonance,wave,waves,normint)*numpy.sin(getPhi(mass,resonance)))
-    return numpy.complex(magV(mass,resonance,wave,waves,normint)*(resonance.w0**2-mass**2)/(resonance.w0**2),magV(mass,resonance,wave,waves,normint)*(resonance.w0*resonance.r0)/(resonance.w0**2))
+    return numpy.complex(magV(mass,resonance,wave,waves,normint)*numpy.cos(getPhi(mass,resonance)+resonance.phase),magV(mass,resonance,wave,waves,normint)*numpy.sin(getPhi(mass,resonance)+resonance.phase))
+    #return numpy.complex(magV(mass,resonance,wave,waves,normint)*(resonance.w0**2-mass**2)/(resonance.w0**2),magV(mass,resonance,wave,waves,normint)*(resonance.w0*resonance.r0)/(resonance.w0**2))
